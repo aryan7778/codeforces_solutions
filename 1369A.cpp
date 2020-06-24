03666c208e3bcdcce330182671b75faf65944078
Aryan Gautam <aryangautam.7778@rediffmail.com>
2020-06-24 10:51:01 +0530

    Create 1369A.cpp

---
+++ 1369A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define fastIO ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+using namespace std;
+
+int main(){
+	fastIO
+	int t;
+	cin>>t;
+	while(t--){
+		long long n;
+		cin>>n;
+		if(n%4==0){
+			cout<<"YES"<<endl;
+			
+		}
+		else{
+			cout<<"NO"<<endl;
+			
+		}
+	}
+	return 0; 
+}
